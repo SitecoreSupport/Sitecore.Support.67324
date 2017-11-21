0fbf675f9962ea247b0b16e7d91919471d77f977
DK\dmsa <dmsa@sitecore.net>
2017-11-21 11:50:54 +0200

    #67324 - Original Code

---
+++ src/Sitecore.Support.67324/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.67324")]
+[assembly: AssemblyProduct("Sitecore.Support.67324")]
+[assembly: ComVisible(false)]
